23e9324ecaa68a8b331527a0f359ce0731f8d0ea
luigazab <luishagailcanonoy@gmail.com>
2026-09-07 18:40:08 +0800

    Update DeviceSelector.jsx
    Changed UI from 4 grid cols into just 1 and its icon from flex-col to same line as button label.

--- src/components/editor/DeviceSelector.jsx
+++ src/components/editor/DeviceSelector.jsx
@@ -8,11 +8,11 @@ const DeviceSelector = ({
   onSelectDevice 
 }) => {
   return (
-    <div data-tour='device-selector' className='flex flex-col justify-center p-1 border-t-2 bg-slate-300 h-full'>
+    <div data-tour='device-selector' className='flex flex-col justify-around h-full p-4'>
       <div className='flex justify-between items-center mb-2'>
-        <h5 className='font-semibold'>Screen Sizes:</h5>
+        <h5 className='font-bold text-slate-600'>Screen Sizes:</h5>
       </div>
-      <div className='grid grid-cols-4 md:grid-cols-2 lg:grid-cols-4 gap-1 m-1'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-1'>
         {Object.entries(deviceSizes).map(([key, device]) => {
           const IconComponent = device.icon;
           const isSelected = responsive && selectedDevice === key;
@@ -20,18 +20,19 @@ const DeviceSelector = ({
             <button 
               key={key} 
               onClick={() => onSelectDevice(key)}
-              className={`flex flex-col items-center rounded-sm btn py-2 px-2 md:py-3 ${
+              className={`flex gap-2 items-center rounded-sm btn py-2 px-2 md:py-3 ${
                 isSelected 
                   ? 'btn-secondary' 
                   : 'btn-lead'
               }`}
             >
-              <IconComponent size={24} />
+              <IconComponent size={20} />
               <span className="text-xs mt-1">{device.label}</span>
             </button>
           );
         })}
       </div>
+      <p className='text-slate-400 text-xs'>Click any button to view your work in different screen sizes.</p>
     </div>
   );
 };
